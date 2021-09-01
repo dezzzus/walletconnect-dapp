228cb908f295b2ddc6c376582b9390a5fa4ed6b1
dezzzus <jacklinsmart@protonmail.com>
2021-09-01 22:57:27 +0800

    test switch request

--- src/Connect.tsx
+++ src/Connect.tsx
@@ -8,8 +8,7 @@ import { useWeb3React, UnsupportedChainIdError } from "@web3-react/core";
 import { InjectedConnector } from "@web3-react/injected-connector";
 import { WalletConnectConnector } from "@web3-react/walletconnect-connector";
 
-const bridge =
-  process.env.REACT_APP_BRIDGE_SERVER || "https://bridge.walletconnect.org";
+const bridge = "https://bridge.walletconnect.org";
 
 const injected = new InjectedConnector({
   supportedChainIds: [1, 3, 4, 5, 42, 56, 97, 137, 80001],
@@ -23,83 +22,89 @@ const ConnectPage = () => {
     if (account && account.length > 0) {
       const web3 = new Web3(library.provider);
       const provider = web3.currentProvider;
-      const msgParams = JSON.stringify({
-        types: {
-          EIP712Domain: [
-            { name: "name", type: "string" },
-            { name: "version", type: "string" },
-          ],
-          Mail: [
-            { name: "Address", type: "address" },
-            { name: "Nonce", type: "string" },
-          ],
-        },
-        primaryType: "Mail",
-        domain: {
-          name: "Privi Pix",
-          version: "1.0.0-beta",
-        },
-        message: {
-          Address: account,
-          Nonce: "0x123456789",
-        },
-      });
-      let params = [account, msgParams];
-      let method = "eth_signTypedData_v3";
-      
-      (provider as any).sendAsync(
-        {
-          method,
-          params,
-          from: account,
-        },
-        function (err: any, result: any) {
-          console.log("sign error", err);
-          if (result.error) {
-            console.log("sign err", result.error);
-          }
-          if (result.result) {
-            console.log("success", result.result);
-            setState("Success: " + result.result);
-          }
-        }
-      );
-      // const isDev = true;
-      // const chainId = isDev ? "0x3" : "0x38";
-      // const rpcUrl = isDev
-      //   ? "https://ropsten.infura.io/v3/eda1216d6a374b3b861bf65556944cdb/"
-      //   : "https://bsc-dataseed.binance.org/";
+      // const msgParams = JSON.stringify({
+      //   types: {
+      //     EIP712Domain: [
+      //       { name: "name", type: "string" },
+      //       { name: "version", type: "string" },
+      //     ],
+      //     Mail: [
+      //       { name: "Address", type: "address" },
+      //       { name: "Nonce", type: "string" },
+      //     ],
+      //   },
+      //   primaryType: "Mail",
+      //   domain: {
+      //     name: "Privi Pix",
+      //     version: "1.0.0-beta",
+      //   },
+      //   message: {
+      //     Address: account,
+      //     Nonce: "0x123456789",
+      //   },
+      // });
+      // let params = [account, msgParams];
+      // let method = "eth_signTypedData_v3";
 
       // (provider as any).sendAsync(
       //   {
-      //     method: "wallet_switchEthereumChain",
-      //     params: [{ chainId }],
+      //     method,
+      //     params,
       //     from: account,
       //   },
       //   function (err: any, result: any) {
-      //     console.log("err", err);
-      //     console.log("result", result);
-      //     if (err && err.code === 4902) {
-      //       (provider as any).sendAsync(
-      //         {
-      //           method: "wallet_addEthereumChain",
-      //           params: [{ chainId, rpcUrl }],
-      //           from: account,
-      //         },
-      //         function (err: any, result: any) {
-      //           console.log("err", err);
-      //           console.log("result", result);
-      //         }
-      //       );
+      //     console.log("sign error", err);
+      //     if (result.error) {
+      //       console.log("sign err", result.error);
+      //     }
+      //     if (result.result) {
+      //       console.log("success", result.result);
+      //       setState("Success: " + result.result);
       //     }
       //   }
       // );
+
+      const isDev = true;
+      const chainId = isDev ? "0x3" : "0x38";
+      const rpcUrl = isDev
+        ? "https://ropsten.infura.io/v3/eda1216d6a374b3b861bf65556944cdb/"
+        : "https://bsc-dataseed.binance.org/";
+
+      (provider as any).sendAsync(
+        {
+          method: "wallet_switchEthereumChain",
+          params: [JSON.stringify({ chainId })],
+          // from: account,
+        },
+        function (err: Error, result: any) {
+          console.log("err", err);
+          console.log("result", result);
+          // setState("Success: " + result.result);
+          // if (err) {
+          //   (provider as any).sendAsync(
+          //     {
+          //       method: "wallet_addEthereumChain",
+          //       params: [{ chainId, rpcUrl }],
+          //       from: account,
+          //     },
+          //     function (err: any, result: any) {
+          //       console.log("err", err);
+          //       console.log("result", result);
+          //     }
+          //   );
+          // }
+        }
+      )
     }
   }, [account, library?.provider]);
 
   const connectWalletConnect = async () => {
     const walletconnect = new WalletConnectConnector({
-      rpc: { 1: process.env.REACT_APP_INFURA_URL || "" },
+      rpc: {
+        1: process.env.REACT_APP_INFURA_URL || "",
+        3: "https://ropsten.infura.io/v3/eda1216d6a374b3b861bf65556944cdb",
+        56: "https://bsc-dataseed.binance.org",
+      },
       bridge,
       qrcodeModal: QRCodeModal,
     });
